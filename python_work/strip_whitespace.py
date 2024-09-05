19ce1de8e30888a492c88b3688eec33f755110e9
emilian-chiper <emilian.chiper.developer@gmail.com>
2024-09-05 22:48:58 +0300

    Strip whitespace from strings

---
+++ python_work/strip_whitespace.py
@@ -0,0 +1,22 @@
+favorite_language = " python "
+
+print(favorite_language) # ' python '
+
+# Remove space from right
+favorite_language = favorite_language.rstrip()
+
+print(favorite_language) # " python"
+
+favorite_language = ' python '
+
+# Remove space from left
+favorite_language = favorite_language.lstrip()
+
+print(favorite_language) # 'python '
+
+favorite_language = ' python '
+
+# Remove space from both sides
+favorite_language = favorite_language.strip()
+
+print(favorite_language) # 'python'
