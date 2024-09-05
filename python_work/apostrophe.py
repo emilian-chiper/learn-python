7f9c931b0bf90d74634b313e559a915e8dc531f1
emilian-chiper <emilian.chiper.developer@gmail.com>
2024-09-05 22:51:48 +0300

    Create syntax error with single quotes and apostrophe

---
+++ python_work/apostrophe.py
@@ -0,0 +1,6 @@
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+# Single quotes cause trouble
+# message = 'One of Python's strengths is its diverse community'
+# print(message) # SyntaxError: unterminated string literal
