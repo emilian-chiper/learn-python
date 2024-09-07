d93d187d50549a3889e07195a05850983d287dce
emiliano <emilian.chiper.developer@gmail.com>
2024-09-07 21:24:55 +0300

    Sort list items
    Used sort() and sort(reverse=True)

---
+++ python_work/03-lists/03-10-cars.py
@@ -0,0 +1,10 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+# Sorting
+cars.sort()
+print(cars)
+
+# Sorting in reverse
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
+print(cars)
