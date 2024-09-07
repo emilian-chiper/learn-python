5abc21737177742c868fa8087eaf2ee1d1a9b86a
emiliano <emilian.chiper.developer@gmail.com>
2024-09-07 21:29:16 +0300

    Organize list
    Used sort(), sort(reverse=True), sorted(), reverse(), and len()
    methods

--- python_work/03-lists/03-10-cars.py
+++ python_work/03-lists/03-10-cars.py
@@ -8,3 +8,23 @@ print(cars)
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 cars.sort(reverse=True)
 print(cars)
+
+# Sort temporarily (doesn't mutate the list)
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print(f"Here is the original list:\n{cars}")
+
+print(f"\nHere is the sorted list:\n\t{sorted(cars)}")
+
+print(f"Here is the original list again:\n{cars}")
+
+# Printing a list in reverse order (i.e. reverse indexing)
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
+
+# Finding the length of a list
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
