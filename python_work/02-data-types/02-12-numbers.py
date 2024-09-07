52eb7988c92cdaca43a52ac25a21970c33cbf952
emiliano <emilian.chiper.developer@gmail.com>
2024-09-07 13:04:46 +0300

    Reorganize repository and work with numbers

---
+++ python_work/02-data-types/02-12-numbers.py
@@ -0,0 +1,46 @@
+# INTEGERS
+
+# Addition
+print(2 + 3) #5
+
+# Subtraction
+print(3 - 2) #1
+
+# Multiplication
+print(2 * 3) #6
+
+# Division
+print(3 / 2) #1.5
+
+# Exponentiation
+print(3 ** 2) #9
+
+# Order of operations
+print(2 + 3 * 4) # 14
+print((2 + 3) * 4) # 20
+
+# FLOATS
+print(0.1 + 0.1) # 0.2
+print(0.2 + 0.2) # 0.4
+print(2 * 0.1) # 0.2
+print(2 * 0.2) # 0
+
+# Arbitrary number decimals
+print(0.2 + 0.1) # 0.30000000000000004
+print(3 * 0.1) # 0.30000000000000004
+
+# INTEGERS AND FLOATS
+print(4 / 2) # 2.0
+print(1 + 2.0) # 3.0
+print(3.0 ** 2) # 9.0
+
+# UNDERSCORES
+universe_age = 14_000_000_000
+print(universe_age) # 14000000000
+
+# MULTIPLE ASSIGNMENTS
+x, y, z = 1, 4, 8
+print(x, y, z) # 1 4 8
+
+# CONSTANTS
+MAX_CONNECTIONS = 5000
---
+++ python_work/02-data-types/02-14-favourite_number.py
@@ -0,0 +1,3 @@
+favourite_number = 409218
+print(f'My favourite number is {favourite_number}')
+# My favourite number is 409218
