b76871b40865a5deaa6e3e1fa2d55a75eebe0d83
emiliano <emilian.chiper.developer@gmail.com>
2024-09-09 15:06:08 +0300

    Move items from one list to another
    While loop runs while unconfirmed_users is not empty

--- python_work/07-user-input-while-loops/72-while-loops/7204-counting.py
+++ python_work/07-user-input-while-loops/72-while-loops/7204-counting.py
@@ -1,11 +1,11 @@
 # Using continue in a Loop
-# current_number = 0
-# while current_number < 10:
-#     current_number += 1
-#     if current_number % 2 == 0:
-#         continue
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
 
-#     print(current_number)
+    print(current_number)
 
 # Avoiding Infinite Loops
 x = 1
