90338023e49cfc96e363bc4f712d7a59c5fdc40c
emiliano <emilian.chiper.developer@gmail.com>
2024-09-10 13:24:31 +0300

    Write a supplement on built-in Python functions

---
+++ python_work/08-functions/built-in_functions.py
@@ -0,0 +1,89 @@
+"""
+
+# CATEGORIES
+Input / Output: print, input, open
+
+Character Encoding: ord, chr
+
+Mathematics: abs, divmod, pow, round, sum
+
+Ordering, max, min, sorted
+
+Collections / Iterations: range, len, reversed, all, any, map,
+                          iter, next
+
+# COMMON BUILT-IN FUNCTIONS
+
+abs(x) << return the absolute value of a function
+
+all(iterable) << return True if bool(e) is True for each element e
+
+any(iterable) << return True if bool(e) is True for at least one element e
+
+chr(int) << return a one-character string with the givve Unicode code point
+
+divmod(x, y) << return(x // y, x % y) as tuple, if x and y are integers
+
+hash(obj) << return an integer hash value for the object
+
+id(obj) << return the unique integer serving as an "identity" for the object
+
+input(prompt) << return a string from stdin; the prompt is optional
+
+isinstance(obj, cls) << determine if obj is an instance of the class
+                        (or a subclass)
+
+iter(iterable) << return a new iterator object for the parameter
+
+len(iterable) << return the # of elems in the given iteration
+
+map(f, iter1, iter2, ...) << return an iterator yielding the result of
+                             function
+                             calls f(e1, e2, ...) for respective elements e1 in
+                             iter1, e2 in iter2, ...
+
+max(iterable) << return the larges element of the given iteration
+
+min(iterable) << return the smallest element of the given iteration
+
+min(a, b, c, ...) << return the smallest of the args
+
+next(iterator) << return the next element reported by the iterator
+
+open(filename, mode) << open a file with the given name or access mode
+
+ord(char) << return the Unicode code point of the given char
+
+pow(x, y) << return x ** y
+
+pow(x, y, z) << return x ** y % z as an integer
+
+print(obj1, obj1, ...) << prints the objects with separating whitespace and
+                          trailing newline
+
+range(stop) << construct an iteration of values 0, 1, ..., stop - 1
+
+range(start, stop) << idem, with specified starting value
+
+range(start, stop, step) << idem, with specified step
+                            (i.e start, start+step, start+2*step)
+
+reversed(sequence) << return an iteration of the sequence in reverse
+
+round(x) << return the nearest int value (a tie is broken to the even value)
+
+round(x, k) << return the value rounded to the nearest 10**-k
+               (return-type matches x) i.e. return x rounded to k digits
+               precisions after the decimal point
+
+sorted(iterable) << return a list containingelements of the iterable is
+                    sorted order
+
+sum(iterable) << retunr the sum of the elements in the iterable (numeric type)
+
+type(obj) << returns the class to which the instance obj belongs
+
+"""
+
+
+
